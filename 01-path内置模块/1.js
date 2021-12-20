130d639e750bcec2faebaf7793313592f36626f7
chenrui668 <1941718132@qq.com>
2021-12-20 21:59:56 +0800

    e

--- 01-path内置模块/1.js
+++ 01-path内置模块/1.js
@@ -15,4 +15,7 @@ console.log(parseObj)
 // let fullpath = path.join(__dirname, '1.js')
 let fullpath = path.join(__dirname, 'test', '2.js') // 多个目录层级用多个参数拼接
 
-console.log(fullpath)
+let fullpath2 = path.resolve('test', 'path2\\p/cc/') // C:\Node_Learn\01-path内置模块\test\path2\p\cc 会将最后的\去掉
+let fullpath3 = path.join('test', 'path2\\p/cc/') // test\path2\p\cc\
+
+console.log(fullpath, fullpath2, fullpath3)
